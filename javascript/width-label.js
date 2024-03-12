aa632b8a4d91b50288f997d37f6dc87af3a698f5
VinothaArivalagan <vinothag96@gmail.com>
2024-03-12 17:26:05 +0100

    make the website responsive for mobile

--- javascript/width-label.js
+++ javascript/width-label.js
@@ -11,3 +11,20 @@ window.addEventListener("resize", () => {
   // Set the text of the element to show the viewport width
   viewportDimensions.innerHTML = `Viewport Width: <b>${width}px</b>`;
 });
+
+
+var element = document.getElementById("hamburger-menu");
+
+var myFunction = function() {
+  var arrayOfElements=document.getElementsByClassName('navbar');
+  let firstItem = arrayOfElements[0];
+  
+  if(firstItem.style.display == "flex"){
+    firstItem.style.display = "none";
+  }
+  else{
+    firstItem.style.display = "flex";
+  }
+};
+
+element.addEventListener("click", myFunction);
